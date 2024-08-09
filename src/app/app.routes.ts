403a23f6b0e8b12904c499fecb89d2e0ead8ed36
leova01 <leovalcarcel01@gmail.com>
2024-08-09 00:23:35 -0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { BrowseComponent } from './pages/browse/browse.component';
 import { DetailsComponent } from './pages/Details/Details.component';
 
 export const routes: Routes = [
-    {path: '**', component: BrowseComponent},
-    {path: '', component: BrowseComponent},
+    
+    {path: '', redirectTo: 'browse', pathMatch: 'full'},
+    {path: 'browse', component: BrowseComponent},
     {path: 'details/:id', component: DetailsComponent},
 ];
